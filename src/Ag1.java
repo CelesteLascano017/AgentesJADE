2f63c9422ed71e44cc691d4fa9457849e0206424
Dumdum <celestelascano2002@gmail.com>
2026-04-26 17:41:48 -0500

    Actualiza Ag1 con comportamiento OneShot

--- src/Ag1.java
+++ src/Ag1.java
@@ -1,8 +1,18 @@
 import jade.core.Agent;
+import jade.core.behaviours.OneShotBehaviour;
 
 public class Ag1 extends Agent{
     @Override
     protected void setup(){
-            System.out.println("Hola, soy el agente: " + getLocalName());
+        System.out.println("Soy el agente: " + getLocalName());
+
+        addBehaviour(new OneShotBehaviour() {
+
+            @Override 
+            public void action(){
+                System.out.println("Estoy ejecutando mi comportamiento");
+            }
+            
+        });
     }
 }
